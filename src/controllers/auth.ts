7f7d87164e0116ac3b4b0bfba4799d43ea58e819
DevShiba <migerushiba@gmail.com>
2024-06-17 22:44:40 -0300

    Get All Jobs

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -17,6 +17,41 @@ export const register = async (req: Request, res: Response) => {
     });
 };
 
-export const login = async (req: Request, res: Response) => {};
+export const login = async (req: Request, res: Response) => {
+    const {email, password} = req.body;
+    if(!email || !password){
+      throw new BadRequestError("Please provide email and password");
+    }
 
-export const updateUser = async (req: Request, res: Response) => {};
+    const user = await User.findOne({email});
+    if(!user){
+        throw new UnauthenticatedError('Invalid Credentials');
+    }
+    const isPasswordCorrect = await user.comparePassword(password);
+    if(!isPasswordCorrect){
+        throw new UnauthenticatedError('Invalid Credentials');
+    }
+
+    const token = user.createJWT();
+
+    res.status(StatusCodes.OK).json({
+      user: {
+        email: user.email,
+        lastName: user.lastName,
+        location: user.location,
+        name: user.name,
+        token,
+      },
+    });
+
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+    const {email, name, lastName, location} = req.body;
+      if (!email || !name || !lastName || !location) {
+        throw new BadRequestError("Please provide all values");
+      }
+
+
+    
+};
--- src/controllers/jobs.ts
+++ src/controllers/jobs.ts
@@ -5,28 +5,64 @@ import mongoose from "mongoose";
 import { Request, Response } from "express";
 //import date-fns
 
-export const getAllJobs = async (req: Request, res: Response) => {
+interface CustomRequest extends Request {
+  user: { userId: string };
+}
+
+export const getAllJobs = async (req: CustomRequest, res: Response) => {
   const { search, status, jobType, sort } = req.query;
 
+  const queryObject: Record<string, any> = {
+    createdBy: req.user.userId,
+  };
 
+  if (search) {
+    queryObject.position = { $regex: search, $options: "i" };
+  }
+
+  if (status && status !== "all") {
+    queryObject.status = status;
+  }
+
+  if (jobType && jobType !== "all") {
+    queryObject.jobType = jobType;
+  }
+
+  let result = Job.find(queryObject);
+
+  if (sort === "latest") {
+    result = result.sort("-createdAt");
+  }
+  if (sort === "oldest") {
+    result = result.sort("createdAt");
+  }
+  if (sort === "a-z") {
+    result = result.sort("position");
+  }
+  if (sort === "z-a") {
+    result = result.sort("-position");
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const jobs = await result;
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
-export const getJob = async(req: Request, res: Response)=>{
+export const getJob = async (req: Request, res: Response) => {};
 
-}
+export const createJob = async (req: Request, res: Response) => {};
 
-export const createJob = async(req: Request, res: Response)=>{
+export const updateJob = async (req: Request, res: Response) => {};
 
-}
+export const deleteJob = async (req: Request, res: Response) => {};
 
-export const updateJob = async(req: Request, res: Response)=>{
-
-}
-
-export const deleteJob = async(req: Request, res: Response)=>{
-
-}
-
-export const showStats = async(req: Request, res: Response)=>{
-
-}
+export const showStats = async (req: Request, res: Response) => {};
